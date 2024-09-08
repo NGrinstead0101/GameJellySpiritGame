9ad9176c73190a3eee890a4370743020270a5fc1
Marissa Moser <marissalmoser@gmail.com>
2024-09-07 22:54:09 -0500

    update continue button

--- Assets/Scripts/UI/MenuInputManager.cs
+++ Assets/Scripts/UI/MenuInputManager.cs
@@ -10,6 +10,8 @@ public class MenuInputManager : MonoBehaviour
     private InputAction _swapToAngel, _swapToDevil, _escape;
 
     [SerializeField] private List<GameObject> _tabs;
+    [SerializeField] private GameObject _continueButton;
+    private bool _canSwap;
 
     private void Awake()
     {
@@ -23,6 +25,8 @@ public class MenuInputManager : MonoBehaviour
         _swapToAngel.performed += ctx => SwapSpiritForm();
         _swapToDevil.performed += ctx => SwapSpiritForm();
         _escape.performed += ctx => EscapePerformed();
+
+        _canSwap = true;
     }
 
     /// <summary>
@@ -30,23 +34,39 @@ public class MenuInputManager : MonoBehaviour
     /// </summary>
     private void EscapePerformed()
     {
-        for(int i = 0; i < _tabs.Count; i++)
+        for (int i = 0; i < _tabs.Count; i++)
         {
             _tabs[i].gameObject.SetActive(false);
         }
-        
+
     }
 
+    /// <summary>
+    /// Swaps menu assets
+    /// </summary>
     private void SwapSpiritForm()
     {
+        if (_canSwap)
+        {
+            _continueButton.SetActive(true);
 
-        if(GameManager.Instance.GetCurrentAbilityType() == AbilitySetType.Angel)
-        {
-            PlayerController.SwapForm?.Invoke(AbilitySetType.Devil);
-        }
-        else
-        {
-            PlayerController.SwapForm?.Invoke(AbilitySetType.Angel);
+            if (GameManager.Instance.GetCurrentAbilityType() == AbilitySetType.Angel)
+            {
+                PlayerController.SwapForm?.Invoke(AbilitySetType.Devil);
+            }
+            else
+            {
+                PlayerController.SwapForm?.Invoke(AbilitySetType.Angel);
+            }
         }
     }
+
+    /// <summary>
+    /// called by the continue button. 
+    /// </summary>
+    public void StopSwap()
+    {
+        _canSwap = false;
+    }
+
 }
